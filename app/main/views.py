c8a09cc0fab4d2f91c49a61cbafc4ce244f2a360
FantomX <panchuk.dmitriy.v@gmail.com>
2024-06-02 15:00:40 +0300

    add home page and controller

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import context
 
-# Create your views here.
+def index(request):
+    context : dict[str,str] = {
+        'title': 'Home',
+        'content' : 'Главная страница'
+    }
+    
+    return render(request, 'main/index.html',context)
+
+
+def about(request):
+    return render(request, 'main/about.html',context)
